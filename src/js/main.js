cda3627e3d022a27dfce745870678e7f0e8a28b8
Tim Scanlin <tscanlin@gmail.com>
2016-04-04 20:00:57 -0700

    add styles and get basic gallery functioning

--- src/js/main.js
+++ src/js/main.js
@@ -3,31 +3,81 @@
   var each = [].forEach;
   var indexOf = [].indexOf;
 
-  function initializePreview(document) {
-    var previewContainer = document.querySelectorAll('.js-preview-container');
-    var previewImage = document.querySelectorAll('.js-preview-image');
+  function initializePreview(document, photos) {
+    var previewContainer = document.querySelector('.js-preview-container');
+    var previewImage = document.querySelector('.js-preview-image');
+    var previewTitle = document.querySelector('.js-preview-title');
+    var previewPrev = document.querySelector('.js-preview-controls--prev');
+    var previewNext = document.querySelector('.js-preview-controls--next');
     var selectors = document.querySelectorAll('[data-box-image]');
+    var hiddenClass = 'hidden';
+
+    var photo = {};
+
+    function findPhoto(photos, url) {
+      return photos.filter(function(photo) {
+        return photo.largeImg === url;
+      })[0] || {};
+    }
+
+    function showPhoto(event, el) {
+      var url = el.getAttribute('data-box-image');
+      var p = findPhoto(photos, url);
+      previewImage.src = url;
+      previewTitle.textContent = p.title;
+      previewImage.onload = function() {
+        previewContainer.classList.remove(hiddenClass);
+      }
+      event.preventDefault();
+    }
+
     document.body.addEventListener('click', function(e) {
-      if (indexOf.call(selectors, e.target) !== -1) {
+      var el = e.target;
+      console.log(e)
+
+      if (el === previewContainer) {
+        previewContainer.classList.add(hiddenClass)
+      }
+
+      if (el.nodeName === 'IMG') {
+        el = el.parentNode;
+      }
+
+      if (indexOf.call(selectors, el) !== -1) {
+        showPhoto(event, el);
+      }
+
+      // Listen for clicks on previous button.
+      if (el === previewPrev) {
 
       }
-    })
+
+    });
+
+    // Listen for close events.
   }
 
   // Tranform Data.
-  function buildURL(photoObj) {
+  function buildURL(photoObj, size) {
+    var ext = '_n.jpg';
+    if (size === 'large') {
+      ext = '_h.jpg';
+    }
     return 'https://c' + photoObj.farm + '.staticflickr.com/' + photoObj.farm
-      + '/' + photoObj.server + '/' + photoObj.id + '_' + photoObj.secret + '_n.jpg'; // "+ _h"
+      + '/' + photoObj.server + '/' + photoObj.id + '_' + photoObj.secret + ext; // "+ _h"
   }
   // https://c4.staticflickr.com/4/3658/3596031283_d41cbca1e2_b.jpg
 
   // Build HTML.
   function buildPhoto(photo, templateHtml) {
     var container = document.createElement('DIV');
-    container.className = 'image-container hidden';
+    container.className = 'inline-block hidden';
     container.innerHTML = templateHtml;
     for (var prop in photo) {
       var el = container.querySelector('[data-prop*="' + prop + '"]')
+      if (!el) {
+        continue;
+      }
       var keyVal = el.getAttribute('data-prop').split(':');
       var key = keyVal[0].trim();
       var val = keyVal[1].trim();
@@ -40,7 +90,7 @@
       // Show el once image loads.
       if (key === 'src') {
         el.onload = function() {
-          el.parentNode.classList.remove('hidden');
+          el.parentNode.parentNode.classList.remove('hidden');
         }
       }
     }
@@ -53,13 +103,22 @@
     var gallery = document.querySelector('.js-gallery');
     var templateHtml = document.querySelector('#image-template').innerHTML;
 
-    each.call(photos, function(photo) {
-      var el = buildPhoto({
+    var arr = [];
+    each.call(photos, function(photo, i) {
+      var data = {
         thumbnailImg: buildURL(photo),
+        largeImg: buildURL(photo, 'large'),
         title: photo.title
-      }, templateHtml);
+      };
+      arr.push(data);
+      var el = buildPhoto(data, templateHtml);
 
       gallery.appendChild(el);
+
+      // At the end.
+      if (i === photos.length - 1) {
+        initializePreview(document, arr);
+      }
     });
   }
 
